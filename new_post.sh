5079f219a81e25e4f7edb7165ca03b3207740218
Furyton <1543159426@qq.com>
2022-03-26 15:25:52 +0800

    add script to create new post

---
+++ new_post.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+echo -n "Post name > "
+read -r REPLY
+title=${REPLY}
+clean_title=`echo $title | tr "[:upper:]" "[:lower:]"]` #Lower Case
+clean_title=`echo $clean_title | iconv -f utf-8 -t ascii//translit` #Remove accents
+clean_title=`echo $clean_title | tr -dc "[a-z0-9 ]"` #Keep spaces, letters and numbers
+clean_title=`echo $clean_title | tr " " "-"` #Replace spaces by dashes
+
+filename=$clean_title.md
+author=`(git config --get user.name)`
+date_pattern=`(date "+%Y-%m-%d")`
+cur_date=$date_pattern
+cat > "posts/"$filename <<EOF
+---
+title: $title
+author: $author
+date: $cur_date
+tags:
+---
+EOF
+
+vim "posts/"$filename
