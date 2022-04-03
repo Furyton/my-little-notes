1dba5b3d9cec2392c9997e9f9b72ee69a2175f28
Furyton <1543159426@qq.com>
2022-04-04 01:07:23 +0800

    add tag list

--- site.hs
+++ site.hs
@@ -1,5 +1,6 @@
 --------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ViewPatterns #-}
 import           Data.Monoid     ((<>))
 import           Hakyll
 import           Slug
@@ -9,6 +10,7 @@ import qualified Data.Text as T
 import           Text.Pandoc.Options
 import           Text.Pandoc.Extensions
 import           Text.Pandoc.Highlighting
+-- import           Text.Blaze.Html5.Attributes as A
 
 --------------------------------------------------------------------------------
 -- | Entry point
@@ -36,27 +38,47 @@ main = hakyllWith config $ do
     match "posts/*" $ do
         route   $ metadataRoute titleRoute
         compile $ pandocCompiler_
-                >>= loadAndApplyTemplate "templates/post.html" (postCtx tags)
+                >>= loadAndApplyTemplate "templates/post.html" (postCtxWithTags tags)
                 -- >>= loadAndApplyTemplate "templates/content.html" defaultContext
                 >>= loadAndApplyTemplate "templates/default.html" defaultContext
                 >>= relativizeUrls
 
     -- Post tags
-    tagsRules tags $ \tag pattern -> do
-        let title = "Posts tagged " ++ tag
+    -- tagsRules tags $ \tag pattern -> do
+    --     let title = "Posts tagged " ++ tag
 
-        -- Copied from posts, need to refactor
+    --     -- Copied from posts, need to refactor
+    --     route idRoute
+    --     compile $ do
+    --         posts <- recentFirst =<< loadAll pattern
+    --         let ctx = constField "title" title <>
+    --                     listField "posts" (postCtx tags) (return posts) <>
+    --                     defaultContext
+    --         makeItem ""
+    --             >>= loadAndApplyTemplate "templates/tag.html" ctx
+    --             -- >>= loadAndApplyTemplate "templates/content.html" ctx
+    --             >>= loadAndApplyTemplate "templates/default.html" ctx
+    --             >>= relativizeUrls
+
+    create ["tags.html"] $ do
+        route idRoute
+        let tagList = tagsMap tags
+        compile $ do
+            makeItem ""
+              >>= loadAndApplyTemplate "templates/tag-list.html" (defaultCtxWithTags tags)
+              >>= loadAndApplyTemplate "templates/default.html" defaultContext
+
+    tagsRules tags $ \tag pat -> do
         route idRoute
         compile $ do
-            posts <- recentFirst =<< loadAll pattern
-            let ctx = constField "title" title <>
-                        listField "posts" (postCtx tags) (return posts) <>
-                        defaultContext
-            makeItem ""
-                >>= loadAndApplyTemplate "templates/tag.html" ctx
-                -- >>= loadAndApplyTemplate "templates/content.html" ctx
-                >>= loadAndApplyTemplate "templates/default.html" ctx
-                >>= relativizeUrls
+            posts <- recentFirst =<< loadAll pat
+            let postCtx = postCtxWithTags tags
+                postsField = listField "posts" postCtx (pure posts)
+                titleField = constField "title" ("Posts tagged \""++tag++"\"")
+                indexCtx = postsField <> titleField <> defaultContext
+            makeItem "" >>= loadAndApplyTemplate "templates/post-list.html" indexCtx
+                        >>= loadAndApplyTemplate "templates/default.html" defaultContext
+                        >>= relativizeUrls
 
     -- Index
     match "index.html" $ do
@@ -64,7 +86,7 @@ main = hakyllWith config $ do
         compile $ do
             posts <- recentFirst =<< loadAll "posts/*"
             let indexContext =
-                    listField "posts" (postCtx tags) (return posts) <>
+                    listField "posts" (postCtxWithTags tags) (return posts) <>
                     field "tags" (\_ -> renderTagList tags) <>
                     defaultContext
 
@@ -78,8 +100,13 @@ main = hakyllWith config $ do
     match "templates/*" $ compile templateCompiler
 
 --------------------------------------------------------------------------------
-postCtx :: Tags -> Context String
-postCtx tags = mconcat
+postCtx :: Context String
+postCtx =
+    dateField "date" "%0Y-%m-%d" `mappend`
+    defaultContext
+
+postCtxWithTags  :: Tags -> Context String
+postCtxWithTags  tags = mconcat
     [ dateField "date" "%B %e, %Y"
     , tagsField "tags" tags
     , Context $ \key -> case key of
@@ -88,6 +115,25 @@ postCtx tags = mconcat
     , defaultContext
     ]
 
+defaultCtxWithTags :: Tags -> Context String
+defaultCtxWithTags tags = listField "tags" tagsCtx getAllTags         <>
+                          defaultContext
+  where getAllTags :: Compiler [Item (String, [Identifier])]
+        getAllTags = mapM (pure . mkItem) $ tagsMap tags
+          where mkItem :: (String, [Identifier]) -> Item (String, [Identifier])
+                mkItem x@(t, _) = Item (tagsMakeId tags t) x
+        tagsCtx :: Context (String, [Identifier])
+        tagsCtx = listFieldWith "posts" postsCtx getPosts             <>
+                  metadataField                                       <>
+                  urlField "url"                                      <>
+                  pathField "path"                                    <>
+                  titleField "title"                                  <>
+                  missingField
+          where getPosts :: Item (String, [Identifier])
+                         -> Compiler [Item String]
+                getPosts (itemBody -> (_, is)) = mapM load is
+                postsCtx :: Context String
+                postsCtx = postCtxWithTags tags
 
 config :: Configuration
 config = defaultConfiguration{ destinationDirectory = "docs"}
@@ -124,3 +170,31 @@ fileNameFromTitle =
 getTitleFromMeta :: Metadata -> String
 getTitleFromMeta =
     fromMaybe "no title" . lookupString "title"
+
+-- postListTemplate :: Template
+-- postListTemplate = readTemplate . renderHtml $ postListTemplateRaw
+
+-- postListTemplateRaw :: Html
+-- postListTemplateRaw =
+--   ul $ do
+--     "$for(posts)$"
+--     li ! A.class_ "" $ do
+--         "$date$: "
+--         a ! href "$url$" $ "$title$"
+--         p ! class_ "taglist" $ "$if(tags)$ Tags: $tags$ $endif$"
+--     "$endfor$"
+
+-- tagListTemplate = readTemplate . renderHtml $ tagListTemplateRaw
+
+-- tagListTemplateRaw :: Html
+-- tagListTemplateRaw =
+--   ul $ do
+--     "$for(tags)$"
+--     li ! A.class_ "" $ do
+--         a ! href "$url$" $ "$title$"
+--         ul $ do
+--           "$for(posts)$"
+--           li ! A.class_ "" $ do
+--             a ! href "$url$" $ "$title$"
+--           "$endfor$"
+--     "$endfor$"
